0030112b08ef0db1d35e6edc63d48c0b040c2987
Muhammad Raza <108022883+maxpii@users.noreply.github.com>
2023-11-27 20:37:30 -0500

    Completed all functionality related work, have to now style it

--- Tic-Tac-Toe/tic-tac-toe.js
+++ Tic-Tac-Toe/tic-tac-toe.js
@@ -2,7 +2,8 @@ let buttons = document.querySelectorAll("button");
 let turn = document.getElementById("turn");
 let current_move = "x";
 turn.innerHTML = `Current Turn: ${current_move.toUpperCase()}`;
-
+let gameEnded = false;
+let gameWinner;
 
 let arr = [
     [0,0,0],
@@ -20,8 +21,121 @@ function changeCurrent() {
 }
 
 for(let i = 0; i < buttons.length; i ++) {
-    buttons[i].addEventListener("click",function() {
+    buttons[i].addEventListener("click",() => {
+        if (!gameEnded) {
+        
+        changeArray(buttons[i]);
+        console.log(arr);
         buttons[i].innerHTML = current_move;
+        checkTest();
         changeCurrent();
+        turn.innerHTML = `Current Turn: ${current_move.toUpperCase()}`;
+        }
+        if (gameEnded) {
+            if (isFull()) {
+                turn.innerHTML = "Tie";
+            }
+            else {
+                turn.innerHTML = `Winner: ${gameWinner.toUpperCase()}`;
+            }
+        }
+
+       
     });
 }
+
+function changeArray(button) {
+    var index = button.id - 1;
+    arr[Math.floor(index/3)][index%3] = current_move;
+}
+function isFull() {
+    for (let i = 0; i < arr.length; i ++) {
+        for (let j = 0; j < arr[i].length; j ++) {
+            if (arr[i][j] === 0) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+// function check() {
+//     if (arr[0][0] === "x" && arr[0][1] === "x" && arr[0][2] === "x") {
+//         gameEnded = true;
+//         gameWinner = "x";
+//     }
+//     else if (arr[1][0] === "x" && arr[1][1] === "x" && arr[1][2] === "x") {
+//         gameEnded = true;
+//         gameWinner = "x";
+//     }
+//     else if (arr[2][0] === "x" && arr[2][1] === "x" && arr[2][2] === "x") {
+//         gameEnded = true;
+//         gameWinner = "x";
+//     }
+//     else if (arr[0][0] === "x" && arr[1][0] === "x" && arr[2][0] === "x") {
+//         gameEnded = true;
+//         gameWinner = "x";
+//     }
+//     else if (arr[0][1] === "x" && arr[1][1] === "x" && arr[2][1] === "x") {
+//         gameEnded = true;
+//         gameWinner = "x";
+//     }
+//     else if (arr[0][2] === "x" && arr[1][2] === "x" && arr[2][2] === "x") {
+//         gameEnded = true;
+//         gameWinner = "x";
+//     }
+//     else if (arr[0][0] === "x" && arr[1][1] === "x" && arr[2][2] === "x") {
+//         gameEnded = true;
+//         gameWinner = "x";
+//     }
+//     else if (arr[0][2] === "x" && arr[1][1] === "x" && arr[2][0] === "x") {
+//         gameEnded = true;
+//         gameWinner = "x";
+//     }
+   
+// }
+
+function checkTest() {
+    if (arr[0][0] === current_move && arr[0][1] === current_move && arr[0][2] === current_move) {
+        gameEnded = true;
+        gameWinner = current_move;
+    }
+    else if (arr[1][0] === current_move && arr[1][1] === current_move && arr[1][2] === current_move) {
+        gameEnded = true;
+        gameWinner = current_move;
+    }
+    else if (arr[2][0] === current_move && arr[2][1] === current_move && arr[2][2] === current_move) {
+        gameEnded = true;
+        gameWinner = current_move;
+    }
+    else if (arr[0][0] === current_move && arr[1][0] === current_move && arr[2][0] === current_move) {
+        gameEnded = true;
+        gameWinner = current_move;
+    }
+    else if (arr[0][1] === current_move && arr[1][1] === current_move && arr[2][1] === current_move) {
+        gameEnded = true;
+        gameWinner = current_move;
+    }
+    else if (arr[0][2] === current_move && arr[1][2] === current_move && arr[2][2] === current_move) {
+        gameEnded = true;
+        gameWinner = current_move;
+    }
+    else if (arr[0][0] === current_move && arr[1][1] === current_move && arr[2][2] === current_move) {
+        gameEnded = true;
+        gameWinner = current_move;
+    }
+    else if (arr[0][2] === current_move && arr[1][1] === current_move && arr[2][0] === current_move) {
+        gameEnded = true;
+        gameWinner = current_move;
+    }
+    else if (isFull()) {
+        gameEnded = true;
+    }
+    
+
+   
+}
+
+
+
+
