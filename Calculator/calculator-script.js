0e81a0e7d5d7a233ebde70ac8b5b76ca3dceaf1e
Muhammad Raza <108022883+maxpii@users.noreply.github.com>
2023-12-29 14:46:52 -0500

    Added a function to all of the buttons. Working on functionality.
    I'm thinking of having master string that would store all of the commands that the user inputs, and then work on the commands from there. This is going to be a little tough

--- Calculator/calculator-script.js
+++ Calculator/calculator-script.js
@@ -1,21 +1,146 @@
+let master_string = "";
+let signs = "+-*/%=";
+
+// heading 
 let heading = document.querySelector('#heading');
+
+// big div
 let calculator = document.querySelector('#calculator');
+
+// screen, paragraph
 let screen = document.querySelector('#screen');
+
+// all buttons
 let clear = document.querySelector("#c");
+clear.addEventListener("click", () => {
+    master_string += clear.innerHTML;
+    doStuff();
+    //console.log(master_string);
+});
+
 let modulo = document.querySelector("#modulo");
+modulo.addEventListener("click", () => {
+    master_string += modulo.innerHTML;
+    doStuff();
+    //console.log(master_string);
+});
+
 let division = document.querySelector("#division");
+division.addEventListener("click", () => {
+    master_string += division.innerHTML;
+    doStuff();
+    //console.log(master_string);
+});
+
+
+
 let seven = document.querySelector("#seven");
+seven.addEventListener("click", () => {
+    master_string += seven.innerHTML;
+    doStuff();
+    //console.log(master_string);
+});
+
 let eight = document.querySelector("#eight");
+eight.addEventListener("click", () => {
+    master_string += eight.innerHTML;
+    doStuff();
+    //console.log(master_string);
+});
+
 let nine = document.querySelector("#nine");
+nine.addEventListener("click", () => {
+    master_string += nine.innerHTML;
+    doStuff();
+    //console.log(master_string);
+});
+
 let multiply = document.querySelector("#multiply");
+multiply.addEventListener("click", () => {
+    master_string += multiply.innerHTML;
+    doStuff();
+    //console.log(master_string);
+});
+
 let four = document.querySelector("#four");
+four.addEventListener("click", () => {
+    master_string += four.innerHTML;
+    doStuff();
+    //console.log(master_string);
+});
+
 let five = document.querySelector("#five");
+five.addEventListener("click", () => {
+    master_string += five.innerHTML;
+    doStuff();
+    //console.log(master_string);
+});
+
 let six = document.querySelector("#six");
+six.addEventListener("click", () => {
+    master_string += six.innerHTML;
+    doStuff();
+    //console.log(master_string);
+});
+
 let minus = document.querySelector("#minus");
+minus.addEventListener("click", () => {
+    master_string += minus.innerHTML;
+    doStuff();
+   //console.log(master_string);
+});
+
 let one = document.querySelector("#one");
+one.addEventListener("click", () => {
+    master_string += one.innerHTML;
+    doStuff();
+    //console.log(master_string);
+});
+
 let two = document.querySelector("#two");
+two.addEventListener("click", () => {
+    master_string += two.innerHTML;
+    doStuff();
+    //console.log(master_string);
+});
+
 let three = document.querySelector("#three");
-let plus = document.querySelector("#+");
+three.addEventListener("click", () => {
+    master_string += three.innerHTML;
+    doStuff();
+    //console.log(master_string);
+});
+
+let plus = document.querySelector("#plus");
+plus.addEventListener("click", () => {
+    master_string += plus.innerHTML;
+    doStuff();
+    //console.log(master_string);
+});
+
 let zero = document.querySelector("#zero");
+zero.addEventListener("click", () => {
+    master_string += zero.innerHTML;
+    doStuff();
+    //console.log(master_string);
+});
+
 let decimal = document.querySelector("#decimal");
+decimal.addEventListener("click", () => {
+    master_string += decimal.innerHTML;
+    doStuff();
+    //console.log(master_string);
+});
+
 let equals = document.querySelector("#equals");
+equals.addEventListener("click", () => {
+    master_string += equals.innerHTML;
+    doStuff();
+    //console.log(master_string);
+});
+// that was exhaustive
+function doStuff () {
+    if (signs.includes(master_string.charAt(0))) {
+        screen.innerHTML = "Can't put a sign right now!";
+    }
+}
