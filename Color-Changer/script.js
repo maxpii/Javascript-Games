50500288acc4b3b9d9c888e34a53ecf3d8d8905a
Muhammad Raza <108022883+maxpii@users.noreply.github.com>
2024-01-05 21:05:34 -0500

    Fixed a few bugs and added more colors
    All the colors work now, and does not give repeated colors.

--- Color-Changer/script.js
+++ Color-Changer/script.js
@@ -1,11 +1,19 @@
 let body = document.querySelector('body');
 let button = document.getElementById("button");
-let arr = ["#1F3A3D","#F5D033","##6C6960","#C93C20","#308446"];
+let arr = ["#1F3A3D","#F5D033","#6C6960","#C93C20"
+,"#308446","#027343","#A0359B","#ECB070","#023101",
+"#01484A","#01484A","#4C4C33","#5F0B0B","#6F117E",
+"#EE2F1C","#1CEE82","#420A17","#84A305","#001C61","#3B453E"];
 
 button.addEventListener("click",changeColor);
-
+let rand;
+let currentColor;
 function changeColor() {
-    let rand = Math.floor(Math.random() * arr.length);
-    button.style.background = arr[rand];
-    body.style.background = arr[rand];
+    rand = Math.floor(Math.random() * arr.length);
+    while(arr[rand] == currentColor) rand = Math.floor(Math.random() * arr.length);;
+
+    currentColor = arr[rand]; 
+    button.style.background = currentColor;
+    body.style.background = currentColor;
+
 }
